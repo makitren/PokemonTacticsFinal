648f2a9f9ccf930f4b0ed784c9a86b67ac0e1fac
makitren <48264164+makitren@users.noreply.github.com>
2020-03-27 12:49:06 +0100

    Demo2
    Demo2

--- ProbandoUnity/Assets/MainCamera.cs
+++ ProbandoUnity/Assets/MainCamera.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class MainCamera : MonoBehaviour
 {
+    public Transform target;
+    public float suavidad;
+
+    private void LateUpdate()
+    {
+        if (transform.position != target.position)
+        {
+            Vector3 posicionObjetivo = new Vector3(target.position.x, target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, posicionObjetivo, suavidad);
+        }
+    }
     // Start is called before the first frame update
-   
+
 }

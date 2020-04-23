98a1ad9728b70e3e4ba1a458d6cc3c63701e5571
makitren <48264164+makitren@users.noreply.github.com>
2020-04-23 15:31:08 +0200

    Actualizacion segundo mapa
    Añadido cosas al segundo mapa

--- ProbandoUnity/Assets/Tiled2Unity/Scripts/Moneda.cs
+++ ProbandoUnity/Assets/Tiled2Unity/Scripts/Moneda.cs
@@ -7,17 +7,22 @@ public class Moneda : MonoBehaviour
     public int moneda = 0;
     public TextMeshProUGUI textoMonedas;
 
+    private void Awake()
+    {
+        GameObject.DontDestroyOnLoad(textoMonedas);
+    }
+
     private void OnTriggerEnter2D(Collider2D otro)
     {
+        
             if (otro.CompareTag("Monedas"))
             {
             moneda++;
             textoMonedas.text = "Monedas: "+moneda.ToString();
            
             Destroy(otro.gameObject);
-           /*
-            DontDestroyOnLoad(textoMonedas);
-            */
+           
+            
         }
     }
 }

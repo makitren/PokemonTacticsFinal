24de240e19e1341ed2747134a5a8f5c5dbe49328
makitren <48264164+makitren@users.noreply.github.com>
2020-04-29 23:33:18 +0200

    Menu pausa introducido
    menu pausa introducido

---
+++ ProbandoUnity/Assets/Tiled2Unity/Scripts/MenuPausa.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+    private bool enPausa;
+    public GameObject panelPausa;
+    public string menuPrincipal;
+    void Start()
+    {
+        enPausa = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("pausa"))
+        {
+            Continuar();
+        }
+    }
+    public void Continuar()
+    {
+        enPausa = !enPausa;
+        if (enPausa)
+        {
+            panelPausa.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            panelPausa.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+    public void SalirAMenu()
+    {
+        SceneManager.LoadScene(menuPrincipal);
+        Time.timeScale = 1f;
+    }
+}

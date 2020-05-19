8ee80c0fe3ce54ad54fb41a14b9ea871511d0b0b
makitren <48264164+makitren@users.noreply.github.com>
2020-05-20 00:28:37 +0200

    Transicion de escena añadida

--- ProbandoUnity/Assets/TransicionEscena.cs
+++ ProbandoUnity/Assets/TransicionEscena.cs
@@ -8,14 +8,66 @@ public class TransicionEscena : MonoBehaviour
     public string escenaCargar;
     public Vector2 posicionJugador;
     public ValorVector memoriaJugador;
+   
+    bool start = false;
+    bool isFadeIn = false;
+    float alpha = 0;
+    float fadeTime = 1f;
+    GameObject mostrarArea;
 
-    public void OnTriggerEnter2D(Collider2D otro)
+
+    void Awake()
+    {
+        
+        FadeOut();
+    }
+
+    IEnumerator OnTriggerEnter2D(Collider2D otro)
     {
         if (otro.CompareTag("Player")&& !otro.isTrigger)
         {
+            otro.GetComponent<Animator>().enabled = false;
+            otro.GetComponent<MoverPersonaje>().enabled = false;
+            FadeIn();
+            yield return new WaitForSecondsRealtime(fadeTime);
+            
             memoriaJugador.valorInicial = posicionJugador;
+           
             SceneManager.LoadScene(escenaCargar);
+            
         }
+    }
+    private void OnGUI()
+    {
+        if (!start)
+            return;
+
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+        Texture2D tex;
+        tex = new Texture2D(1, 1);
+        tex.SetPixel(0, 0, Color.black);
+        tex.Apply();
+
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), tex);
+
+        if (isFadeIn)
+        {
+            alpha = Mathf.Lerp(alpha, 1.1f, fadeTime * Time.deltaTime);
+        }
+        else
+        {
+            alpha = Mathf.Lerp(alpha, -0.1f, fadeTime * Time.deltaTime);
+            if (alpha < 0) start = false;
+        }
+    }
+    void FadeIn()
+    {
+        start = true;
+        isFadeIn = true;
+    }
+    void FadeOut()
+    {
+        isFadeIn = false;
     }
 
 }

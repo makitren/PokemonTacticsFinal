5a9317ad3484bc8ddefd08d1f8d480473a070cee
makitren <48264164+makitren@users.noreply.github.com>
2020-05-25 04:19:33 +0200

    Añadido puertas
    Se ha añadido puertas que funcionan con las llaves de los cofres

---
+++ ProbandoUnity/Assets/TextMesh Pro/Tiled2Unity/Scripts/Puerta.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TipoPuerta
+{
+    llave,
+    boton
+}
+public class Puerta : MonoBehaviour
+{
+    [Header(" Variables de puerta")]
+    public Senial interrogacionOn;
+    public Senial interrogacionOff;
+    public TipoPuerta tipoPuerta;
+    public bool abierta = false;
+    public bool jugadorEnRango = false;
+    public InventarioJugador inventarioJugador;
+    public SpriteRenderer spritePuerta;
+    public BoxCollider2D fisicasColision;
+
+ 
+
+    public void Abrir()
+    {
+        spritePuerta.enabled = false;
+        abierta = true;
+        fisicasColision.enabled = false;
+    }
+    public void Cerrar()
+    {
+
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (jugadorEnRango&& tipoPuerta == TipoPuerta.llave)
+            {
+                if (inventarioJugador.numeroLlaves > 0)
+                {
+                    inventarioJugador.numeroLlaves--;
+                    Abrir();
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D otro)
+    {
+        if (otro.CompareTag("Player"))
+        {
+            interrogacionOn.Raise();
+            jugadorEnRango = true;
+
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D otro)
+    {
+        if (otro.CompareTag("Player"))
+        {
+            interrogacionOff.Raise();
+            jugadorEnRango = false;
+            StopAllCoroutines();
+        }
+    }
+}
